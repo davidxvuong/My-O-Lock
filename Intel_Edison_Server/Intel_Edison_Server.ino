a53e2d3438c1ea96714d74c2270402d0af80de55
Denny Hoang <dennyhoang5@gmail.com>
2015-05-09 22:37:45 -0400

    DH / MD - Client logic and response handling (Server Client Untested)

--- Intel_Edison_Server/Intel_Edison_Server.ino
+++ Intel_Edison_Server/Intel_Edison_Server.ino
@@ -1,10 +1,86 @@
+#include <WiFi.h>
+
+#define MAX_BUFFER_SIZE 50
+
+// TODO: Fill in Mac address
+byte mac[] = { 0
+};
+
+IPAddress ip(192, 188, 43, 3);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
+
+WiFiServer server(23);
+
+char *ssid = "The D";
+char *pass = "Microsoft1328";
+
+char wifi_client_buffer[MAX_BUFFER_SIZE] = {'\0'};
+int wifi_client_buffer_top_index = 0;
+
+boolean alreadyConnected = false; 
 
 void setup() {
-  // put your setup code here, to run once:
+  WiFi.begin(ssid, pass);
+  server.begin();
+  
+  Serial.begin(9600);
+}
 
+int get_wifi_client_input_until_cr(WiFiClient &client, char *buff, int buff_size) {
+  
+  int i;
+  for(i = 0; i < buff_size && client.available() > 0; i++) {
+    
+    char c = client.read(); 
+    wifi_client_buffer[i] = c;
+    wifi_client_buffer_top_index++;
+    if('\r' == c) {
+      return i+1;
+    }
+  }
+  return i;
+}
+
+void flush_wifi_client_input_buffer() {
+  memset(wifi_client_buffer, '\0', wifi_client_buffer_top_index);
+  wifi_client_buffer_top_index = 0;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  WiFiClient client = server.available();
+  if (client) {
+    if (!alreadyConnected) {
+      client.flush();    
+      alreadyConnected = true;
+    }
+    
+    if (client.available() > 0) {
+      int chars_copied = get_wifi_client_input_until_cr(client, &wifi_client_buffer[0], MAX_BUFFER_SIZE);
+      
+      //command has been entered; process it
+      if('\r' == wifi_client_buffer[wifi_client_buffer_top_index]) {
+        
+        char *tok = strtok(wifi_client_buffer, "\r");
+        
+        if (NULL != tok) {
+          
+          if(0 == strcmp(wifi_client_buffer, "open")) {
+            //Success
+          }
+          
+          else if(0 == strcmp(wifi_client_buffer, "close")) {
+            //Failure
+          }
+          
+          //not sure what this command is
+          else {
+            
+          }
+        }
+        
+        flush_wifi_client_input_buffer();
+      }
+    }
+  }
 }
